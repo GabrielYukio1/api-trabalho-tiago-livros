edd0f7b3b8620f067402b15294ffa5baf2f94025
GabrielYukio1 <gabrielyukio77@gmail.com>
2026-04-15 23:13:38 -0300

    fix index require server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require("./app");
+const app = require("./server");
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
